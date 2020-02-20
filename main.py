a660cdd259000e5bd5e567bd926a18d47cd38f2c
Matthew C-R <s1438675@ed.ac.uk>
2020-02-20 18:08:49 +0000

    AppMag to AbMag

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ s1438675@ed.ac.uk
 
 # Imports
 from astropy.io import fits
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -21,26 +22,49 @@ def foldcurve(_inputarray, _period):
     # Update x axis to phase
     for i in range(0, _inputarray.shape[0]):
         _inputarray[i, 0] = ((_inputarray[i, 0] - _epoch) / _period) % 1
-    # Return new array
+    # Return updated array
     return _inputarray
 
 
+# Apparent magnitude to absolute magnitude
+def appmagnitudetoabsmagnitude(_inputarray, _pmas):
+    # Distance in parsecs is 1 / parallax half angle
+    _distance = 1 / (_pmas / 2)
+    # Iterate through array
+    for i in range(0, _inputarray.shape[0]):
+        # Update magnitude from apparent magnitude to absolute magnitude
+        _inputarray[i, 0] =_inputarray[i, 0] - (5 * (math.log10(_distance))) + 5
+    # Return updated array
+    return _inputarray
+
 # Plot array using pyplot
 def plotarray(_inputarray):
+    # Create a new array twice the size of the input to plot from phase 0 -> 2
     _plotarray = np.zeros((_inputarray.shape[0] * 2, _inputarray.shape[1]), dtype=float)
-
+    # Iterate through array
     for i in range(0, _plotarray.shape[0]):
+        # Before phase 1 simply copy the data
         if i < _inputarray.shape[0]:
             _plotarray[i] = _inputarray[i]
+        # After phase 1 shift all by +1
         else:
             _plotarray[i] = _inputarray[i - _inputarray.shape[0]]
             _plotarray[i][0] = _plotarray[i][0] + 1
-
+    # Set pyplot stype
     plt.style.use('seaborn-whitegrid')
     plt.errorbar(_plotarray[:, 0], _plotarray[:, 1], yerr=_plotarray[:, 2], fmt='.k')
+    # Set axis values to the limits of the data array, considering error bar size
+    axes = plt.gca()
+    axes.set_xlim([0, 2])
+    axes.set_ylim([np.amin(_plotarray[:, 1], axis=0) - np.amax(_plotarray[:, 2], axis=0),
+                   np.amax(_plotarray[:, 1], axis=0) + np.amax(_plotarray[:, 2], axis=0)])
+    # Label x axis
     plt.xlabel("Phase")
+    # Label y axis
     plt.ylabel("Magnitude")
+    # Flip y axis as convention
     plt.gca().invert_yaxis()
+    # Show plot
     plt.show()
 
 # Main function
@@ -58,9 +82,11 @@ def main(_filename, _period):
                 _kbandarray = np.append(_kbandarray, np.array([_row[0], _row[1], _row[2]], ndmin=2), axis=0)
         # Delete blank first entry
         _kbandarray = np.delete(_kbandarray, 0, axis=0)
+
+        #_kbandarray = appmagnitudetoabsmagnitude(_kbandarray, 0.5187)
         _kbandarray = foldcurve(_kbandarray, _period)
         plotarray(_kbandarray)
 
 
 # Program entry point
-main('Data/858994112734.fits', 0.236334)
+main('Data/858994114024.fits', 0.558301)
